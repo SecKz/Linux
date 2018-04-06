32b7a2045d4e8094c3965758cc80627f3934c5d7
huangjing <260741887@qq.com>
2018-04-06 18:20:01 +0800

    ccccc

--- centos/py36u.sh
+++ centos/py36u.sh
@@ -1,18 +1,13 @@
 #!/bin/sh
 # ubuntu 安装Python 3.6
-# http://www.cnblogs.com/lilidun/p/6041198.html
-#! /usr/bin/python3 -Es
-
-# buntu14.04上安装python3.6
-# 使用这些命令安装就行，如果没有add-apt-repository命令。先安装 apt-get install software-properties-common
-#  add-apt-repository ppa:jonathonf/python-3.6
-#  apt-get update
-#  apt-get install python3.6
-
+#! head /usr/bin/lsb_release	查看lsb_release使用的python版本
 
 cd /root
 [ -e "Python-3.6.4.tar.xz" ] || wget --no-check-certificate https://www.python.org/ftp/python/3.6.4/Python-3.6.4.tar.xz
 [ -d "Python-3.6.4" ] && rm -rf "Python-3.6.4"
+
+[ -f /root/lsb_release ] || cp /usr/bin/lsb_release /root/lsb_release
+
 tar xf Python-3.6.4.tar.xz
 cd Python-3.6.4
 
